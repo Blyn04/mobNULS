99f8a48e362ddbbeb52ff41d038bbe55a97e6a40
berlene <berlenebernabe12@gmail.com>
2025-04-12 18:25:40 +0800

    ADMIN INVENTORY CONNECTED TO DB

--- components/styles/InventoryStyle.js
+++ components/styles/InventoryStyle.js
@@ -7,42 +7,6 @@ export default StyleSheet.create({
     padding: 16,
   },
 
-  /* Header Section */
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    backgroundColor: '#00796B',
-    padding: 15,
-    borderRadius: 8,
-    justifyContent: 'space-between',
-  },
-
-  logo: {
-    width: 40,
-    height: 40,
-    resizeMode: 'contain',
-  },
-
-  headerText: {
-    alignItems: 'center',
-    flex: 1,
-  },
-
-  title: {
-    color: 'white',
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-
-  subtitle: {
-    color: 'white',
-    fontSize: 14,
-  },
-
-  profileButton: {
-    padding: 5,
-  },
-
   /* Section Title */
   sectionTitle: {
     fontSize: 20,
